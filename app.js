f320daf088bfbf46b5871e5bcab6f673f4ffc9ee
neebjames <neebjames@gmail.com>
2020-03-30 15:41:03 +0530

    added favicon

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+app.use(express.favicon());
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to Node.js & Express' });
 });
