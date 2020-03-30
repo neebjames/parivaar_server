3021e0ec2f47ed3201fba987a42501653e6a512e
neebjames <neebjames@gmail.com>
2020-03-30 14:34:48 +0530

    corrected babel

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-
+app.get('/', (req, res) => {
+  res.send('Welcome to Node.js & Express');
+});
 app.use('/', parivaarAPI);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
