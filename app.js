b432bb0b17f78297639e2df9e70459ffd7610152
neebjames <neebjames@gmail.com>
2020-03-30 16:53:01 +0530

    removed api

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import cors from 'cors';
-const parivaarAPI = require('./routes/parivaarAPI');
+var parivaarAPI = require('./routes/parivaarAPI');
 const app = express();
 
 app.use(logger('dev'));
