e3a7d7c6c9c0d10b19926ac884ea327ef31cb85f
neebjames <neebjames@gmail.com>
2020-03-30 16:11:13 +0530

    babel

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 // Transpile all code following this line with babel and use '@babel/preset-env' (aka ES6) preset.
 require('babel-register')({
-    presets: [ 'es2015' ]
+    presets: [ 'env' ]
  });
  
 require('./app.js')
